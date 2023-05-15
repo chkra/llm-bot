cf0b57a7d90e5579a8200549017336a4110e7219
Christina Kratsch <9607474+chkra@users.noreply.github.com>
2023-05-15 10:47:31 +0200

    Fixed error due to change in import procedures

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 #imports
 from flask import Flask, render_template, request
-from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader
+#from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader
+from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex
+from llama_index.node_parser import SimpleNodeParser
 import os
 
 os.environ['OPENAI_API_KEY'] = "your api key here"
+
 
 app = Flask(__name__)
 
 #create chatbot
 documents = SimpleDirectoryReader('htw-data').load_data()
-index = GPTSimpleVectorIndex(documents)
+parser = SimpleNodeParser()
+nodes = parser.get_nodes_from_documents(documents)
+index = GPTVectorStoreIndex(nodes)
 
 #define app routes
 @app.route("/")
